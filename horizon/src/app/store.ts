018c01be845fb07f92dc9aeef79f6798e3bbcc0b
CrimsonUnicorn <96168390+CrimsonUnicorn@users.noreply.github.com>
2026-07-23 05:33:52 +0000

    FE-09.4	Provider Allocation Audits

--- horizon/src/app/store.ts
+++ horizon/src/app/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import settingsReducer from "../features/Settings/settingSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    settings: settingsReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
