cd2f38c937a0bf652255a9017ebb14a95c2cb28a
CrimsonUnicorn <96168390+CrimsonUnicorn@users.noreply.github.com>
2026-07-24 08:29:17 +0000

    FE-11.1,FE-11.3

---
+++ horizon/src/services/api.ts
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://api.example.com',
+    timeout: 10000,
+})
+
+api.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+api.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    (error) => {
+        const status = error.res?.status;
+
+        switch (status) {
+            case 401:
+                console.error("Unauthorized");
+                break;
+
+            case 403:
+                console.error("Forbidden");
+                break;
+
+            case 404:
+                console.error("Resource not found");
+                break;
+
+            case 500:
+                console.error("Internal server error");
+                break;
+
+            default:
+                console.error("Something went wrong");
+        }
+        return Promise.reject(error);
+    }
+)
+
+
+
+export default api;
